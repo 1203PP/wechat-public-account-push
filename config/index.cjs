f9f89173dc522c39b9cbfc579913e53f935c02a7
多吃萝卜身体好 <114289366+1203PP@users.noreply.github.com>
2023-12-02 00:10:13 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oxIas6Pq8BfnDEVb7vbqA4XN2k20',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'a7fitBobtZmWPE7HfbWT7vNigAxEaQ8_qd_8AzJ9RfE',
+      useTemplateId: '4m6v8I7-qQqxyntXFwNwf8BiXvHqTM2UXyH7rKp28k0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-03',
       festivals: [
