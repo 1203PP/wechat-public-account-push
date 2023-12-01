0fc7e27f327092086ad72c98f35761e6dbd129f0
多吃萝卜身体好 <114289366+1203PP@users.noreply.github.com>
2023-12-02 00:03:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -46,7 +46,8 @@ const USER_CONFIG = {
 
  SLOT_LIST: [
     // 这样配置的话，就会每次发送这句话
-    { keyword: 'encourage_oneself', contents: '你主要的问题在于读书太少而想得太多' },
+    { keyword: 'encourage_oneself', contents: '每天说点没意义的话才是有意义的生活-爱你哟' },
+     ],
    // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
   CALLBACK_TEMPLATE_ID: '',
 
